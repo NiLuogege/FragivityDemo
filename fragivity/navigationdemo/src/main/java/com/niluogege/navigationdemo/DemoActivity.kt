3016fe98b9e6aabaaadeb16586c13606fb61609f
luoChen <hank.luo@xianghuanji.com>
2021-02-25 16:32:56 +0800

    navigation 基本跳转 ok 了

--- fragivity/navigationdemo/src/main/java/com/niluogege/navigationdemo/DemoActivity.kt
+++ fragivity/navigationdemo/src/main/java/com/niluogege/navigationdemo/DemoActivity.kt
@@ -2,10 +2,19 @@ package com.niluogege.navigationdemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 
 class DemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_demo)
+
+//        val navHostFragment =
+//            supportFragmentManager.findFragmentById(R.id.nav_host) as NavHostFragment
+//        val navController = navHostFragment.navController
+
+
+
+//        navController.na
     }
 }
